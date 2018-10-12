0a7e1a8f4112c0d0fe244a38baa8f0c78ccf96bf
Demo User <debian@beaglebone.localdomain>
2018-10-12 20:19:03 +0000

    More stepper testing

--- stepper_test.py
+++ stepper_test.py
@@ -5,20 +5,29 @@ import time
 
 rcpy.set_state(rcpy.RUNNING)
 
-#states = [ (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0) ]
-states = [ (1, -1), (-1, -1), (-1, 1), (1, 1)]
+states = [ (1, -1), (0, -1), (-1, -1), (-1, 0), (-1, 1), (0, 1), (1, 1), (1, 0) ]
+#states = [ (1, -1), (-1, -1), (-1, 1), (1, 1)]
 
+direction = 1
+direction_count = 0
+step_interval = .003
+reverse_count = 800
 try:
-    t0 = time.time() + 1
+    t0 = time.time() + step_interval
     count = 0
     while True:
         if (time.time() >= t0):
             motor.motor1.set(states[count][0])
-            print('Switching motor state:', states[count][0], states[count][1])
             motor.motor2.set(states[count][1])
-            count +=1
-            if count == 4:
+            count += direction
+            if count == len(states):
                 count = 0
-            t0 = time.time() + 1
+            if count == -1:
+                count = len(states) -1
+            t0 = time.time() + step_interval
+            direction_count +=1
+            if direction_count >= reverse_count:
+                direction = -direction
+                direction_count = 0
 finally:
     rcpy.exit()
